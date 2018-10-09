a84e9ed9592e7a5c87777c3c986764e0bb70462a
Amber Kumar <ambkumar@ucdavis.edu>
2018-10-09 14:23:37 -0700

    add zodiac check

---
+++ zodiac-check/js/index.js
@@ -0,0 +1,197 @@
+const horoscope = {
+  leo: {
+    imageLink:
+      "http://www.astrology-zodiac-zodiacSigns.com/zodiac-zodiacSigns/aquarius/",
+    meaning:
+      'People born under the sign of Leo are natural born leaders. They are dramatic, creative, self-confident, dominant and extremely difficult to resist, able to achieve anything they want to in any area of life they commit to. There is a specific strength to a Leo and their "king of the jungle" status. Leo often has many friends for they are generous and loyal. Self-confident and attractive, this is a Sun sign capable of uniting different groups of people and leading them as one towards a shared cause, and their healthy sense of humor makes collaboration with other people even easier.'
+  },
+  virgo: {
+    imageLink:
+      "http://www.astrology-zodiac-zodiacSigns.com/zodiac-zodiacSigns/virgo/",
+    meaning:
+      "Virgos are always paying attention to the smallest details and their deep sense of humanity makes them one of the most careful signs of the zodiac. Their methodical approach to life ensures that nothing is left to chance, and although they are often tender, their heart might be closed for the outer world. This is a sign often misunderstood, not because they lack the ability to express, but because they won’t accept their feelings as valid, true, or even relevant when opposed to reason. The symbolism behind the name speaks well of their nature, born with a feeling they are experiencing everything for the first time."
+  },
+  libra: {
+    imageLink:
+      "http://www.astrology-zodiac-zodiacSigns.com/zodiac-zodiacSigns/libra/",
+    meaning:
+      "People born under the sign of Libra are peaceful, fair, and they hate being alone. Partnership is very important for them, as their mirror and someone giving them the ability to be the mirror themselves. These individuals are fascinated by balance and symmetry, they are in a constant chase for justice and equality, realizing through life that the only thing that should be truly important to themselves in their own inner core of personality. This is someone ready to do nearly anything to avoid conflict, keeping the peace whenever possible. Planet ruling the sign of Libra is Venus, making these people great lovers but also fond of expensive, material things. Their lives need to be enriched by music, art, and beautiful places they get a chance to visit. "
+  },
+  scorpio: {
+    imageLink:
+      "http://www.astrology-zodiac-zodiacSigns.com/zodiac-zodiacSigns/scorpio/",
+    meaning:
+      "Scorpio-born are passionate and assertive people. They are determined and decisive, and will research until they find out the truth. Scorpio is a great leader, always aware of the situation and also features prominently in resourcefulness. Scorpio is a Water sign and lives to experience and express emotions. Although emotions are very important for Scorpio, they manifest them differently than other water signs. In any case, you can be sure that the Scorpio will keep your secrets, whatever they may be."
+  },
+  sagittarius: {
+    imageLink:
+      "http://www.astrology-zodiac-zodiacSigns.com/zodiac-zodiacSigns/sagittarius/",
+    meaning:
+      "Curious and energetic, Sagittarius is one of the biggest travelers among all zodiac signs. Their open mind and philosophical view motivates them to wander around the world in search of the meaning of life. Sagittarius is extrovert, optimistic and enthusiastic, and likes changes. Sagittarius-born are able to transform their thoughts into concrete actions and they will do anything to achieve their goals. Like the other fire signs, Sagittarius needs to be constantly in touch with the world to experience as much as possible. The ruling planet of Sagittarius is Jupiter, the largest planet of the zodiac. Their enthusiasm has no bounds, and therefore people born under the Sagittarius sign possess a great sense of humor and an intense curiosity."
+  },
+  capricorn: {
+    imageLink:
+      "http://www.astrology-zodiac-zodiacSigns.com/zodiac-zodiacSigns/capricon/",
+    meaning:
+      "Capricorn is a sign that represents time and responsibility, and its representatives are traditional and often very serious by nature. These individuals possess an inner state of independence that enables significant progress both in their personal and professional lives. They are masters of self-control and have the ability to lead the way, make solid and realistic plans, and manage many people who work for them at any time. They will learn from their mistakes and get to the top based solely on their experience and expertise. "
+  },
+  aquarius: {
+    imagelink:
+      "http://www.astrology-zodiac-zodiacSigns.com/zodiac-zodiacSigns/aquarius/",
+    meaning:
+      "Aquarius-born are shy and quiet , but on the other hand they can be eccentric and energetic. However, in both cases, they are deep thinkers and highly intellectual people who love helping others. They are able to see without prejudice, on both sides, which makes them people who can easily solve problems. Although they can easily adapt to the energy that surrounds them, Aquarius-born have a deep need to be some time alone and away from everything, in order to restore power. People born under the Aquarius sign, look at the world as a place full of possibilities."
+  },
+  pisces: {
+    imageLink:
+      "http://www.astrology-zodiac-zodiacSigns.com/zodiac-zodiacSigns/pisces/",
+    meaning:
+      "Pisces are very friendly, so they often find themselves in a company of very different people. Pisces are selfless, they are always willing to help others, without hoping to get anything back. Pisces is a Water sign and as such this zodiac sign is characterized by empathy and expressed emotional capacity. Their ruling planet is Neptune, so Pisces are more intuitive than others and have an artistic talent. Neptune is connected to music, so Pisces reveal music preferences in the earliest stages of life. They are generous, compassionate and extremely faithful and caring."
+  },
+  aries: {
+    imageLink:
+      "http://www.astrology-zodiac-zodiacSigns.com/zodiac-zodiacSigns/aries/",
+    meaning:
+      "As the first sign in the zodiac, the presence of Aries always marks the beginning of something energetic and turbulent. They are continuously looking for dynamic, speed and competition, always being the first in everything - from work to social gatherings. Thanks to its ruling planet Mars and the fact it belongs to the element of Fire (just like Leo and Sagittarius), Aries is one of the most active zodiac signs. It is in their nature to take action, sometimes before they think about it well. The Sun in such high dignity gives them excellent organizational skills, so you will rarely meet an Aries who is not capable of finishing several things at once, often before lunch break! Their challenges show when they get impatient, aggressive and vent anger pointing it to other people. Strong personalities born under this sign have a task to fight for their goals, embracing togetherness and teamwork through this incarnation."
+  },
+  taurus: {
+    imageLink:
+      "http://www.astrology-zodiac-zodiacSigns.com/zodiac-zodiacSigns/taurus/",
+    meaning:
+      "Practical and well-grounded, Taurus is the sign that harvests the fruits of labor. They feel the need to always be surrounded by love and beauty, turned to the material world, hedonism, and physical pleasures. People born with their Sun in Taurus are sensual and tactile, considering touch and taste the most important of all senses. Stable and conservative, this is one of the most reliable signs of the zodiac, ready to endure and stick to their choices until they reach the point of personal satisfaction. "
+  },
+  gemini: {
+    imageLink:
+      "http://www.astrology-zodiac-zodiacSigns.com/zodiac-zodiacSigns/gemini/",
+    meaning:
+      "The sign of Gemini belongs to the element of Air, accompanying Libra and Aquarius, and this connects it to all aspects of the mind. It is ruled by Mercury, the planet that represents communication, writing, and movement. People born under this Sun sign often have a feeling that their other half is missing, so they are forever seeking new friends, mentors, colleagues and people to talk to. Their changeable and open mind makes them excellent artists, especially writers and journalists, and their skills and flexibility make them shine in trade, driving and team sports. This is a versatile, inquisitive, fun loving sign, born with a wish to experience everything there is out there, in the world. This makes their character inspiring, and never boring."
+  },
+  cancer: {
+    imageLink:
+      "http://www.astrology-zodiac-zodiacSigns.com/zodiac-zodiacSigns/cancer/",
+    meaning:
+      "Deeply intuitive and sentimental, Cancer can be one of the most challenging zodiac signs to get to know. They are very emotional and sensitive, and care deeply about matters of the family and their home. Cancer is sympathetic and attached to people they keep close. Those born with their Sun in Cancer are very loyal and able to empathize with other people's pain and suffering. Lack of patience or even love will manifest through mood swings later in life, and even selfishness, self-pity or manipulation. They are quick to help others, just as they are quick to avoid conflict, and rarely benefit from close combat of any kind, always choosing to hit someone stronger, bigger, or more powerful than they imagined. When at peace with their life choices, Cancer representatives will be happy and content to be surrounded by a loving family and harmony in their home."
+  }
+};
+
+const input = () => {
+  let day = document.getElementById("day").value;
+  let month = document.getElementById("month");
+  let monthName = month.options[month.selectedIndex].text;
+  let monthNumber = month.options[month.selectedIndex].value;
+
+  // check for empty fields
+  if (month.selectedIndex == 0 || !day) {
+    swal("Invalid", "Please Fill all required fields", "error");
+    clear();
+    return;
+  }
+
+  //check if the inputed day is lesser or equals 0
+  if (day <= 0) {
+    swal(
+      "Oops!",
+      "Day value is Invalid. Enter a valid day between 1 - 31",
+      "error"
+    );
+    clear();
+    return;
+  }
+
+  let dateInstance = new Date(2018, monthNumber, 0);
+  console.log(dateInstance);
+
+  // Throw an exception if day entered is more than days in the actual month
+  if (day > dateInstance.getDate()) {
+    let exceptionMessage = `${monthName.toUpperCase()} is not up to ${day} days.\n\n ${monthName.toUpperCase()} has only ${dateInstance.getDate()} days`;
+    swal("Oops!", exceptionMessage, "error");
+    clear();
+    return;
+  }
+
+  // let horoscopeDay = Number(day);
+  // let horoscopeMonth = Number(monthNumber);
+
+  checkHoroscope(Number(day), Number(monthNumber));
+  console.log(Number(day) + " :   " + Number(monthNumber));
+};
+
+const checkHoroscope = (day, month) => {
+  let zodiacSign;
+
+  switch (month) {
+    case 1:
+      zodiacSign = day < 20 ? "capricorn" : "aquarius";
+      break;
+
+    case 2:
+      zodiacSign = day < 19 ? "aquarius" : "pisces";
+      break;
+
+    case 3:
+      zodiacSign = day < 21 ? "pisces" : "aries";
+      break;
+
+    case 4:
+      zodiacSign = day < 20 ? "aries" : "taurus";
+      break;
+
+    case 5:
+      zodiacSign = day < 21 ? "taurus" : "gemini";
+      break;
+
+    case 6:
+      zodiacSign = day < 21 ? "gemini" : "cancer";
+      break;
+
+    case 7:
+      zodiacSign = day < 23 ? "cancer" : "leo";
+      break;
+
+    case 8:
+      zodiacSign = day < 23 ? "leo" : "virgo";
+      break;
+
+    case 9:
+      zodiacSign = day < 23 ? "virgo" : "libra";
+      break;
+
+    case 10:
+      zodiacSign = day < 23 ? "libra" : "scorpio";
+      break;
+
+    case 11:
+      zodiacSign = day < 22 ? "scorpio" : "sagittarius";
+      break;
+
+    case 12:
+      zodiacSign = day < 22 ? "sagittarius" : "capricorn";
+      break;
+
+    default:
+      zodiacSign = "hello";
+      break;
+  }
+
+  let quickTellerResult = ` 
+                            Your Horoscope is ${zodiacSign.toUpperCase()}\n
+                            INFO : ${horoscope[zodiacSign].meaning}
+                            `;
+  swal("🔮", quickTellerResult, "success");
+  // swal({
+  //     title : quickTellerResult,
+  //     text : "<img src='" + horoscope[zodiacSign].imagelink + "' style='width:150px;'>",
+  //     html : true
+  // })
+  clear();
+};
+
+// ${horoscope[zodiacSign].imageLink}\n
+
+// Add an event listener on button click
+let submit = document.getElementById("submitBtn");
+submit.addEventListener("click", input);
+
+const clear = () => {
+  document.getElementById("day").value = "";
+  document.getElementById("month").selectedIndex = 0;
+};
